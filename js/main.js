9481af564e6774a44b0190546ea6a8825e329c3e
Mayur Jadhav <jadhav.mayur135@gmail.com>
2023-04-02 20:21:36 +0530

    Added the missed artifacts

--- js/main.js
+++ js/main.js
@@ -110,3 +110,18 @@
     
 })(jQuery);
 
+$(document).ready(function(){
+    $(".fancybox").fancybox({
+          openEffect: "none",
+          closeEffect: "none"
+      });
+      
+      $(".zoom").hover(function(){
+          
+          $(this).addClass('transition');
+      }, function(){
+          
+          $(this).removeClass('transition');
+      });
+  });
+
